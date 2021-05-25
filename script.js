f57b2423e0ad668775543179aa6a394899baca81
Joel Spinelli <joel.spinelli@hotmail.com>
2021-05-25 13:30:38 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+let pendingDevs = [
+  { name: 'Bruno', display: 'Bruno' },
+  { name: 'Herivelton', display: 'Herivelton' },
+  { name: 'Biguzzi', display: 'Biguzzi' },
+  { name: 'Joel', display: 'Joel' },
+  { name: 'Quik', display: 'Quik' },
+  { name: 'Titi', display: 'Titi' },
+  { name: 'Filipe', display: 'Filipe' },
+  { name: 'Wisky', display: 'Wisky' },
+  { name: 'W2', display: 'W2' }
+];
+
+let devSpeaking = {};
+
+let pendingList = document.querySelector('[pending-list]');
+
+let removeChild = function (dev) {
+  if(!!devSpeaking.display) {
+    let retiredList = document.querySelector('[retired-list]');
+    let retiredDiv = document.createElement('div');
+    let attri = document.createAttribute('class');
+    attri.value = 'col-md-3 dev rounded-circle text-center';
+    retiredDiv.setAttributeNode(attri);
+    retiredDiv.innerText = devSpeaking.display;
+    retiredList.appendChild(retiredDiv);
+
+  }
+
+  devSpeaking = dev;
+  let divDev = document.querySelector('[dev-speaking]');
+  divDev.innerHTML = devSpeaking.name;
+}
+
+function displayPedingsDevs() {
+  pendingDevs.forEach( d => {
+    let devDiv = document.createElement('div');``
+    let attr = document.createAttribute('class');
+
+    attr.value = 'col-md-3 dev rounded-circle text-center';
+
+    devDiv.innerText = d.display;
+    devDiv.setAttributeNode(attr);
+    devDiv.onclick = function() {
+      removeChild(d);
+      this.parentElement.removeChild(this);
+    }
+
+    pendingList.appendChild(devDiv);
+  });
+}
+
+
+displayPedingsDevs();
