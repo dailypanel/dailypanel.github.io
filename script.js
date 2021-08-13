269889348db109b64e0558b6ea1ab8a093b47bbe
Joel Spinelli <joel.spinelli@hotmail.com>
2021-08-13 11:08:05 -0300

    add random button to first user

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let pendingDevs = [
+const devList = [
   { name: 'Bruno', display: 'Bruno' },
   { name: 'Herivelton', display: 'Herivelton' },
   { name: 'Biguzzi', display: 'Biguzzi' },
@@ -10,28 +10,41 @@ let pendingDevs = [
   { name: 'Denis', display: 'Denis' }
 ];
 
+let devsToSpeak = devList;
 let devSpeaking = {};
+let retiredList = document.querySelector('[retired-list]');
 let pendingList = document.querySelector('[pending-list]');
 let divDevSpeaking = document.querySelector('[dev-speaking]');
 
-let removeChild = function (dev) {
+let removeChild = function (dev, index) {
+  // debugger
   if(!!devSpeaking.display) {
-    let retiredList = document.querySelector('[retired-list]');
     let retiredDiv = document.createElement('div');
     let attri = document.createAttribute('class');
+
     attri.value = 'col-md-3 dev text-center';
     retiredDiv.setAttributeNode(attri);
     retiredDiv.innerText = devSpeaking.display;
     retiredList.appendChild(retiredDiv);
-
   }
-
+  
+  devsToSpeak.splice(index, 1);
   devSpeaking = dev;
   divDevSpeaking.innerHTML = devSpeaking.name;
 }
 
+// o primeiro ainda não está removendo
+
+divDevSpeaking.addEventListener('click', function() {
+  if(!!devSpeaking.name) { return; }
+  let rndInt = Math.floor(Math.random() * devsToSpeak.length);
+  removeChild(devsToSpeak[rndInt], rndInt);
+  displayPedingsDevs();
+});
+
 function displayPedingsDevs() {
-  pendingDevs.forEach( d => {
+  pendingList.innerHTML = '';
+  devsToSpeak.forEach( (d, i) => {
     let devDiv = document.createElement('div');
     let attr = document.createAttribute('class');
 
@@ -40,12 +53,14 @@ function displayPedingsDevs() {
     devDiv.innerText = d.display;
     devDiv.setAttributeNode(attr);
     devDiv.onclick = function() {
-      removeChild(d);
+      devsToSpeak.splice(i, 1);
+      removeChild(d, i);
       this.parentElement.removeChild(this);
     }
 
     pendingList.appendChild(devDiv);
   });
 }
+
 
 displayPedingsDevs();
