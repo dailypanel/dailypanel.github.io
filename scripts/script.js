b44080532e61da750d240391c5911994e911611e
fernando.watanabe <fyukio.wata@gmail.com>
2022-02-05 20:00:43 -0300

    new layout

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+let pendingDevs = [
+  { name: 'Biguzzi', display: 'Biguzzi' },
+  { name: 'Joel', display: 'Joel' },
+  { name: 'Filipe', display: 'Filipe' },
+  { name: 'Erivelto', display: 'Erivelto' },
+  { name: 'Watanabe', display: 'Watanabe' }
+];
+
+let devSpeaking = {};
+let pendingList = document.querySelector('[pending-list]');
+let divDevSpeaking = document.querySelector('[dev-speaking]');
+
+let removeChild = function (dev) {
+  if(!!devSpeaking.display) {
+    let retiredList = document.querySelector('[retired-list]');
+    let retiredDiv = document.createElement('div');
+    let attri = document.createAttribute('class');
+    attri.value = 'col-md-3 dev text-center';
+    retiredDiv.setAttributeNode(attri);
+    retiredDiv.innerText = devSpeaking.display;
+    retiredList.appendChild(retiredDiv);
+
+  }
+
+  devSpeaking = dev;
+  divDevSpeaking.innerHTML = devSpeaking.display;
+}
+
+function displayPedingsDevs() {
+  pendingDevs.forEach( d => {
+    let devDiv = document.createElement('div');
+    let attr = document.createAttribute('class');
+
+    attr.value = 'col-md-3 dev text-center';
+
+    devDiv.innerText = d.display;
+    devDiv.setAttributeNode(attr);
+    devDiv.onclick = function() {
+      removeChild(d);
+      this.parentElement.removeChild(this);
+    }
+
+    pendingList.appendChild(devDiv);
+  });
+}
+
+displayPedingsDevs();
